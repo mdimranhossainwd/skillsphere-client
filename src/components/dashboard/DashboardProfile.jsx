80f01a0bade1f0458eb0087401dbb20e30390282
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-06-06 08:55:00 +0600

    Profile Sections Added

--- src/components/dashboard/DashboardProfile.jsx
+++ src/components/dashboard/DashboardProfile.jsx
@@ -1,16 +1,18 @@
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css";
 import useAuth from "../../hooks/useAuth";
 import Profile from "../../shared/Profile";
-
 const DashboardProfile = () => {
   const { user } = useAuth();
-  console.log(user);
+
   return (
-    <div>
+    <div className="">
       <Profile
         heading={user?.displayName}
         img={user?.photoURL}
         email={user?.email}
       />
+      {/* <Calendar date={new Date()} /> */}
     </div>
   );
 };
--- src/shared/Profile.jsx
+++ src/shared/Profile.jsx
@@ -1,15 +1,14 @@
 const Profile = ({ img, name, email, phone, role, heading }) => {
   return (
     <div>
-      <div className="my-12 pl-12">
-        <h3 className="text-2xl italic font-bold">
-          Welcome back,{" "}
-          <span className="bg-gradient-to-r from-[#76c893] to-blue-500 text-transparent bg-clip-text">
-            {heading}
-          </span>
-        </h3>
-
-        <div className="my-16 bg-white py-14 rounded-[44px] px-12 w-1/2 text-center">
+      <div className="my-12 w-full">
+        <div className="my-16 bg-white py-10 rounded-[44px]  px-10 text-center">
+          <h3 className="text-2xl mb-6 italic font-bold">
+            Welcome back,{" "}
+            <span className="bg-gradient-to-r from-[#76c893] to-blue-500 text-transparent bg-clip-text">
+              {heading}
+            </span>
+          </h3>
           <img src={img} className="w-44 mx-auto rounded-full mb-6" alt="" />
           <div>
             <h5 className="italic text-md mb-3 font-bold text-[#4361ee]">
