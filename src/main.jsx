79125278bc4697e6ac5cca96f56b0886835d3857
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-10-25 08:56:51 +0600

    Install to Tanstack Package

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
@@ -5,11 +6,15 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Router";
 import AuthProvider from "./components/provider/AuthProvider";
 import "./index.css";
+
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Toaster />
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
