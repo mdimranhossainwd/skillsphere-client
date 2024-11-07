d7eb5715f3154b1e2c34ad1a7d137e1a2f7cfb93
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-11-07 17:21:29 +0600

    ui: some improves to dashboard menu colors

--- src/components/DashboardMenu.jsx
+++ src/components/DashboardMenu.jsx
@@ -6,65 +6,48 @@ const DashboardMenu = () => {
 
   const menu = (
     <>
-      <>
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
-          to="profile"
-        >
-          My Profile
-        </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
+        to="profile"
+      >
+        My Profile
+      </NavLink>
 
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
-          to="add-product"
-        >
-          Add Product
-        </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
+        to="add-product"
+      >
+        My Class
+      </NavLink>
 
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
-          to="my-product"
-        >
-          My Products
-        </NavLink>
-      </>
-      <>
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
-          to="review-queue"
-        >
-          Review Queue
-        </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
+        to="my-product"
+      >
+        Certificate
+      </NavLink>
 
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
-          to="reported"
-        >
-          Reported Contents
-        </NavLink>
-      </>
-      <>
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
-          to="payment"
-        >
-          Payment History
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
-          to="manage-user"
-        >
-          Manage User's
-        </NavLink>
-      </>
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
+        to="review-queue"
+      >
+        Education
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (isActive ? "text-[#28b485]" : "")}
+        to="payment"
+      >
+        Payment History
+      </NavLink>
     </>
   );
 
   return (
     <div>
       <aside className="md:w-64 min-h-screen bg-[#3f3f3f] p-4">
-        <div className="flex items-center mb-6">
-          <h3 className="text-2xl text-center flex items-center justify-center font-josefin mt-5 bg-gradient-to-r from-blue-700 via-blue-400 to-blue-300 bg-clip-text text-transparent font-bold ">
+        <div className="flex items-center">
+          <h3 className="text-2xl text-center flex items-center justify-center font-josefin mt-5 text-[#5C53FE] font-bold ">
             SkillSphere
           </h3>
         </div>
