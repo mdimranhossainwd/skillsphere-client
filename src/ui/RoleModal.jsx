c60ae860eeadf9e5295e971e2fc5d3234abe6659
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-11-28 18:53:47 +0600

    func: Changes role modal ui completed

--- src/ui/RoleModal.jsx
+++ src/ui/RoleModal.jsx
@@ -5,20 +5,31 @@ const RoleModal = ({ isOpen, setIsOpen, item }) => {
     <>
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
         <div>
-          <h2 className="text-2xl font-semibold mb-3">
+          <h2 className="text-2xl font-semibold mb-2">
             Change the Member Role
           </h2>
-          <p className="text-md font-medium text-gray-700">
+          <p className="text-[15px] font-medium text-gray-700">
             Changing user's role changes their API token access.
           </p>
-          <div>
+          <div className="flex items-center gap-4 pb-6 mt-5">
             <img
               src={item?.photo}
               alt={item?.name || "User Image"}
               className="w-16 h-16 rounded-full"
             />
-            <p className="text-lg font-semibold">{item?.name}</p>
-            <p className="text-sm text-gray-600">{item?.email}</p>
+            <div>
+              <p className="text-lg font-semibold">{item?.name}</p>
+              <p className="text-sm text-gray-600">{item?.email}</p>
+            </div>
+          </div>
+          <hr />
+          <div className="flex items-center justify-between mt-4">
+            <button className="hover:bg-gradient-to-r bg-gray-600 hover:from-primary hover:to-secondary py-2 px-5 font-medium text-white rounded-full">
+              Instructor
+            </button>
+            <button className="bg-gradient-to-r hover:from-primary hover:to-secondary bg-gray-600 py-2 px-7 font-medium text-white rounded-full">
+              Admin
+            </button>
           </div>
         </div>
       </Modal>
