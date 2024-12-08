9d84d0df55fb15f92614cf1ecf75fb736d01bc5f
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-12-08 14:22:34 +0600

    func: Instructor add his course assainment successfully

--- src/components/form/AssainmentForm.jsx
+++ src/components/form/AssainmentForm.jsx
@@ -1,4 +1,32 @@
+import toast from "react-hot-toast";
+import useAuth from "../../hooks/useAuth";
+import useAxios from "../../hooks/useAxios";
+
 const AddAssignmentForm = () => {
+  const { user } = useAuth();
+  const axios = useAxios();
+  // Add Assainment Functions
+
+  const addAssainment = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title?.value;
+    const course = form.course?.value;
+    const dueDate = form.dueDate?.value;
+    const description = form.description?.value;
+    const email = user?.email;
+    const formAllData = { title, course, dueDate, description, email };
+
+    try {
+      const { data } = await axios.post("/add-assainments", formAllData);
+      toast.success("wooow ! successfully add this assainments");
+    } catch (err) {
+      toast.err("Something is wrong");
+    }
+
+    console.log(formAllData);
+  };
+
   return (
     <>
       <div className="max-w-6xl mx-auto p-6 rounded-lg">
@@ -6,7 +34,7 @@ const AddAssignmentForm = () => {
           Add Assainments
         </h2>
         <div className="w-full">
-          <form>
+          <form onSubmit={addAssainment}>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
               {/* Assignment Title */}
               <div className="mb-4">
@@ -37,12 +65,12 @@ const AddAssignmentForm = () => {
                   className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-primary"
                 >
                   <option value="">Select Course</option>
-                  <option value="course1">Data Structure</option>
-                  <option value="course2">Machine Learning</option>
-                  <option value="course3">Frontend</option>
-                  <option value="course3">Backend</option>
-                  <option value="course3">Devops</option>
-                  <option value="course3">Ethical Hacking</option>
+                  <option value="data-structure">Data Structure</option>
+                  <option value="machine-learning">Machine Learning</option>
+                  <option value="frontend">Frontend</option>
+                  <option value="backend">Backend</option>
+                  <option value="devops">Devops</option>
+                  <option value="ethical-hacking">Ethical Hacking</option>
                 </select>
               </div>
 
