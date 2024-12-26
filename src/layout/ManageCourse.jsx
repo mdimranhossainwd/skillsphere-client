bfdb17235e78fc6a33fc33986b60a65ebb068512
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-12-26 08:55:13 +0600

    ui: All Course manage pages completed

--- src/layout/ManageCourse.jsx
+++ src/layout/ManageCourse.jsx
@@ -24,6 +24,89 @@ const ManageCourse = () => {
         <h2 className="text-4xl font-arima pt-10 text-center font-bold">
           All Courses
         </h2>
+        <div className="px-2 md:px-5 mt-7">
+          <table className="table w-full">
+            <thead className="w-full rounded-md text-white bg-gradient-to-r from-secondary via-green-400 to-primary text-center">
+              <tr className="text-lg font-pt">
+                <th className="py-[10px]"></th>
+                <th className="py-[10px]">Course Name</th>
+                <th className="py-[10px]">Instructor</th>
+                <th className="py-[10px]">Category</th>
+                <th className="py-[10px]">Status</th>
+                <th className="py-[10px]">Price</th>
+                <th className="py-[10px]">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="w-full items-center text-center ">
+              {getDataInfo?.map((item, index) => (
+                <tr key={item._id} className="border-b">
+                  <td className="px-4 py-2 flex font-medium justify-between text-center">
+                    {index + 1}. {item?.transId}
+                  </td>
+                  <td className="px-4 py-2 font-medium">{item?.title}</td>
+                  <td className="px-4 py-2 font-medium">{item?.instructor}</td>
+                  <td className="px-4 py-2 font-medium">{item?.category}</td>
+                  <td
+                    className={`badge  font-medium ${
+                      item.status === "Accepted" &&
+                      "bg-emerald-100/60 text-emerald-500 -mt-4"
+                    } ${
+                      item.status === "Pending" &&
+                      "bg-blue-100/60 text-blue-500 -mt-[10px]"
+                    } ${
+                      item.status === "Rejected" &&
+                      "bg-red-100/60 text-red-500 -mt-4"
+                    }`}
+                  >
+                    {item.status === "Accepted"
+                      ? "Active"
+                      : item.status === "Rejected"
+                      ? "Inactive"
+                      : item.status}
+                  </td>
+
+                  <td className="px-4 py-2 font-medium">{item?.price}</td>
+                  <td className="flex gap-4 items-center justify-center">
+                    <button>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="lucide lucide-check"
+                      >
+                        <path d="M20 6 9 17l-5-5" />
+                      </svg>
+                    </button>
+                    <button>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="lucide lucide-circle-x"
+                      >
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="m15 9-6 6" />
+                        <path d="m9 9 6 6" />
+                      </svg>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
