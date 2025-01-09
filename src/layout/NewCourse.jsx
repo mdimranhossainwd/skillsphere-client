47712cb87895eb2cd0103df48c66520dd442e34e
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-01-09 15:18:55 +0600

    ui: acceptable/recommand data card showing

--- src/layout/NewCourse.jsx
+++ src/layout/NewCourse.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import CourseCard from "../components/card/CourseCard";
 import useAxios from "../hooks/useAxios";
 
 const NewCourse = () => {
@@ -23,6 +24,12 @@ const NewCourse = () => {
           <h3 className="text-2xl font-bold px-3 md:px-0">
             Recommended to you based on ratings
           </h3>
+
+          <div className="grid grid-cols-5 gap-3 -mt-2">
+            {getCourses?.map((item) => (
+              <CourseCard key={item?._id} course={item} />
+            ))}
+          </div>
         </div>
       </div>
     </>
