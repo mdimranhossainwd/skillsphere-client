222780ccb43e9ede5f120a437fe5b47fc5289c33
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-01-10 11:10:15 +0600

    data: load user's payment data

--- src/layout/UserPaymentHistory.jsx
+++ src/layout/UserPaymentHistory.jsx
@@ -1,4 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../hooks/useAuth";
+import useAxios from "../hooks/useAxios";
+
 const UserPaymentHistory = () => {
+  const { user } = useAuth();
+  const axios = useAxios();
+
+  const getData = async () => {
+    const { data } = await axios.get(`/payment?email=${user?.email}`);
+    return data;
+  };
+
+  const { data: getPaymentData, refetch } = useQuery({
+    queryKey: ["getPaymentData"],
+    queryFn: getData,
+  });
+
+  console.log(getPaymentData);
+
   return (
     <>
       <div className="my-8">
