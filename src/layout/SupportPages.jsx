70bb06fd0dd5743043691816fed74719db6590db
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-11 14:12:08 +0600

    func: spport data posted on db added

--- src/layout/SupportPages.jsx
+++ src/layout/SupportPages.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 import useAuth from "../hooks/useAuth";
+import useAxios from "../hooks/useAxios";
 
 const SupportPages = () => {
+  const axios = useAxios();
   const { user } = useAuth();
   const [formData, setFormData] = useState({
     name: "",
@@ -14,12 +17,20 @@ const SupportPages = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const submittedData = {
       ...formData,
       email: user?.email || "",
     };
+
+    try {
+      const { data } = await axios.post("/support", submittedData);
+      toast.success("Ticket Submitted Successfully");
+    } catch (err) {
+      console.log(err);
+    }
+
     console.log("Form Submitted", submittedData);
   };
 
