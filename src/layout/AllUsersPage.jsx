badf960102c9ef7b9ab68768e9f728bdaecb888d
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-11 13:41:06 +0600

    All-User's pages paginations done

--- src/layout/AllUsersPage.jsx
+++ src/layout/AllUsersPage.jsx
@@ -1,21 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useAxios from "../hooks/useAxios";
 import RoleModal from "../ui/RoleModal";
 
 const AllUsersPage = () => {
+  const [itemsPerPage, setItemsPerPage] = useState(2);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [count, setCount] = useState(0);
+  const [product, setProduct] = useState([]);
+  const [filter, setFilter] = useState("");
   const axios = useAxios();
   const [isOpen, setIsOpen] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
 
-  const getAllUsers = async () => {
-    const { data } = await axios.get("/users");
-    return data;
-  };
+  // const getAllUsers = async () => {
+  //   const { data } = await axios.get("/users");
+  //   return data;
+  // };
 
   const { data: getUserInfo, refetch } = useQuery({
-    queryKey: ["getUserInfo"],
-    queryFn: getAllUsers,
+    queryKey: ["getUserInfo", currentPage, itemsPerPage, filter],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `/users?pages=${currentPage}&size=${itemsPerPage}}`
+      );
+      return data;
+    },
   });
 
   console.log(getUserInfo);
@@ -25,6 +35,23 @@ const AllUsersPage = () => {
 
     setSelectedUser(user);
     setIsOpen(true);
+  };
+
+  useEffect(() => {
+    const getCount = async () => {
+      const { data } = await axios.get(`/users-count`);
+      setCount(data.count);
+    };
+    getCount();
+  }, []);
+
+  const numberOfPages = Math.ceil(count / itemsPerPage);
+  const pages = [...Array(numberOfPages).keys()].map(
+    (elements) => elements + 1
+  );
+
+  const handlePaginationButton = (value) => {
+    setCurrentPage(value);
   };
 
   return (
@@ -94,6 +121,73 @@ const AllUsersPage = () => {
               ))}
             </tbody>
           </table>
+        </div>
+        <div className="flex justify-center mt-12">
+          {/* Previous Button */}
+          <button
+            disabled={currentPage === 1}
+            onClick={() => handlePaginationButton(currentPage - 1)}
+            className="px-4 py-2 mx-1 transition-colors duration-300 transform bg-primary rounded-md hover:bg-secondary disabled:hover:bg-gray-200  hover:text-white disabled:cursor-not-allowed text-white"
+          >
+            <div className="flex items-center -mx-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M7 16l-4-4m0 0l4-4m-4 4h18"
+                />
+              </svg>
+
+              <span className="mx-1">previous</span>
+            </div>
+          </button>
+          {/* Numbers */}
+          {pages.map((btnNum) => (
+            <button
+              onClick={() => handlePaginationButton(btnNum)}
+              key={btnNum}
+              className={`hidden ${
+                currentPage === btnNum
+                  ? "bg-gradient-to-r from-secondary via-green-400 to-primary text-white"
+                  : ""
+              } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline  hover:bg-secondary hover:text-white`}
+            >
+              {btnNum}
+            </button>
+          ))}
+
+          {/* Next Button */}
+          <button
+            disabled={currentPage === numberOfPages}
+            onClick={() => handlePaginationButton(currentPage + 1)}
+            className="px-4 py-2 mx-1 transition-colors duration-300 transform bg-primary rounded-md hover:bg-secondary disabled:hover:bg-gray-200  hover:text-white disabled:cursor-not-allowed text-white"
+          >
+            <div className="flex items-center -mx-1">
+              <span className="mx-1">Next</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </div>
+          </button>
         </div>
       </div>
     </>
